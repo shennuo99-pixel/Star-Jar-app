0c230224da8583ff51cb785c1c0baa0ebba35b26
shennuo99-pixel <shennuo99@icloud.com>
2026-05-09 11:23:01 +0800

    Use dynamic import for App to completely delay Firebase initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
 const renderApp = () => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
+  import('./App.tsx').then(({ default: App }) => {
+    ReactDOM.createRoot(document.getElementById('root')!).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    )
+  });
 };
 
 // 注册 Firebase 代理 Service Worker
